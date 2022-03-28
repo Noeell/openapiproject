c86590a0d2b24efb261481285eaa3746dc4f84b0
Noel <noel.gebhardt@gmail.com>
2022-03-28 08:28:32 +0200

    FormelOneApplication

--- src/main/java/ch/bbw/m226/openapiproject/FormelOneApplication.java
+++ src/main/java/ch/bbw/m226/openapiproject/FormelOneApplication.java
@@ -7,20 +7,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PonyApplication {
+public class FormelOneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PonyApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer configurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*")
-						.allowedOrigins("*");
-			}
-		};
+		SpringApplication.run(FormelOneApplication.class, args);
 	}
 }
