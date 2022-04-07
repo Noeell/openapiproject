fd28d360d7d030b2f2ff4f98a917d77be503bc11
Noel <noel.gebhardt@gmail.com>
2022-04-07 21:34:19 +0200

    insert with teams & Loading.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import {Link, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Teams from "./Teams";
 import Drivers from "./Drivers";
 import './Header.css';
 
 export default function App() {
 
-
-
-
     return (
         <Routes>
             <Route exact path="/drivers" element={<Drivers/>}/>
             <Route exact path="/" element={<Drivers/>}/>
             <Route exact path="/teams" element={<Teams/>}/>
-
         </Routes>
     );
-
 }
--- src/Drivers.js
+++ src/Drivers.js
@@ -1,29 +1,53 @@
-import {Button, Col, Container, Navbar, Row} from "react-bootstrap";
+import {Button, Col, Container, Row} from "react-bootstrap";
 import Header from "./Header";
 import {DataGrid} from '@mui/x-data-grid';
-import {driversUuidDelete, getDrivers, getDriverByUuid} from "./ApiForDrivers";
+import {addNewDriver, driversUuidDelete, getDriverByUuid, getDrivers, getRacingTeams} from "./ApiForDrivers";
 import {useEffect, useState} from "react";
+import {Card, FormControl, InputLabel, MenuItem, Select, TextField} from "@mui/material";
+import Loading from "./Loading";
 
 function Drivers() {
     const [drivers, setDrivers] = useState([]);
+    const [racingTeams, setRacingTeams] = useState([]);
+    const [selectedRacingTeam, setSelectedRacingTeam] = useState({});
     const [selectedRow, setSelectedRow] = useState();
     const [editedCell, setEditedCell] = useState();
+    const [insertPressed, setInsertPressed] = useState();
+    const [pageSize, setPageSize] = useState(5);
+
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+    const [age, setAge] = useState();
+    const [country, setCountry] = useState();
+    const [points, setPoints] = useState();
+
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         loadAllEntries()
     }, [])
 
+    useEffect(() => {
+        getRacingTeams()
+            .then(data => setRacingTeams(data))
+            .catch(error => console.error(error));
+    }, [insertPressed])
+
     function loadAllEntries() {
+        setIsPending(true);
+        console.log(drivers);
         getDrivers()
-            .then(data => setDrivers(data))
+            .then(data => {
+                setIsPending(false);
+                setDrivers(data);
+            })
             .catch(error => console.error(error));
     }
 
-    console.log(drivers);
     const columns = [
         {field: 'uuid', headerName: 'Id', width: 350},
-        {field: 'name', headerName: 'First name', width: 150, editable: true},
-        {field: 'prename', headerName: 'Last name', width: 150, editable: true},
+        {field: 'prename', headerName: 'First name', width: 150, editable: true},
+        {field: 'name', headerName: 'Last name', width: 150, editable: true},
         {
             field: 'age',
             headerName: 'Age',
@@ -39,6 +63,37 @@ function Drivers() {
         }
     ];
 
+    function insertEntry() {
+        if (firstName?.trim() !== "" && lastName?.trim() !== "" && age !== 0 && country?.trim() !== "" && firstName !== undefined && lastName !== undefined && country !== undefined) {
+            let newDriver;
+            if (selectedRacingTeam.country !== undefined) {
+                newDriver = {
+                    name: lastName,
+                    prename: firstName,
+                    age: age,
+                    points: points,
+                    country: country,
+                    racingTeam: selectedRacingTeam
+                }
+            } else {
+                newDriver = {
+                    name: lastName,
+                    prename: firstName,
+                    age: age,
+                    points: points,
+                    country: country,
+                }
+            }
+
+            addNewDriver(newDriver)
+                .catch(error => console.error(error))
+                .finally(loadAllEntries);
+            setInsertPressed(false);
+            setSelectedRacingTeam({});
+        }
+
+    }
+
     function deleteRow(listOfUuid) {
         for (let i = 0; i < listOfUuid.length; i++) {
             driversUuidDelete(listOfUuid[i])
@@ -47,7 +102,7 @@ function Drivers() {
         }
     }
 
-    function updateRow(){
+    function updateRow() {
         let driverData;
         console.log(selectedRow)
         getDriverByUuid(selectedRow)
@@ -55,32 +110,37 @@ function Drivers() {
             .catch(error => console.error(error));
 
         console.log(driverData)
+        console.log(editedCell)
     }
+
     return (
         <>
-            <Header/>
+            <Header/><br/>
             <Container>
                 <Row>
                     <Col>
                         <h2>Drivers</h2>
                     </Col>
-                </Row><br/>
+                </Row><br/>{isPending && <Loading/>}
                 <Row>
-                    <DataGrid
+                    {drivers.length > 0 && <DataGrid
                         rows={drivers}
                         columns={columns}
+                        pageSize={pageSize}
+                        onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
                         rowsPerPageOptions={[5, 10, 20]}
+                        pagination
                         checkboxSelection
-                        autoHeight
                         autoWidth
+                        autoHeight
                         getRowId={row => row.uuid}
                         onSelectionModelChange={itm => setSelectedRow(itm)}
-                        onEditRowsModelChange={dd => setEditedCell(dd)}
-                    />
+                        onEditRowsModelChange={dd => console.log(dd)}
+                    />}
                 </Row><br/>
                 <Row>
                     <Col>
-                        <Button>Insert</Button>
+                        <Button onClick={() => setInsertPressed(true)}>Insert</Button>
                     </Col>
                     <Col>
                         <Button onClick={updateRow}>Update</Button>
@@ -88,12 +148,38 @@ function Drivers() {
                     <Col>
                         <Button onClick={() => deleteRow(selectedRow)}>Delete</Button>
                     </Col>
-                </Row>
-
+                </Row><br/><br/>
+                {insertPressed && <Row>
+                    <Card sx={{maxWidth: 250}}><br/>
+                        <h4>New entry:</h4>
+                        <TextField id="standard-basic" label="First name" variant="standard"
+                                   onChange={e => setFirstName(e.target.value)}/>
+                        <TextField id="standard-basic" label="Last name" variant="standard"
+                                   onChange={e => setLastName(e.target.value)}/>
+                        <TextField id="standard-basic" label="age" variant="standard" type={"number"}
+                                   onChange={e => setAge(e.target.value)}/>
+                        <TextField id="standard-basic" label="Country" variant="standard"
+                                   onChange={e => setCountry(e.target.value)}/>
+                        <TextField id="standard-basic" label="Points" variant="standard" type={"number"}
+                                   onChange={e => setPoints(e.target.value)}/><br/><br/>
+                        <InputLabel variant={"standard"}>Racing Team</InputLabel>
+                        <FormControl variant={"standard"} fullWidth>
+                            <Select>
+                                <MenuItem onClick={() => setSelectedRacingTeam({})}>
+                                    None
+                                </MenuItem>
+                                {racingTeams.map((team, idx) => <MenuItem onClick={() => setSelectedRacingTeam(team)}
+                                                                          key={idx}
+                                                                          value={team}>{team.name}</MenuItem>)}
+                            </Select>
+                        </FormControl>
+                        <br/><br/><br/>
+                        <Button onClick={insertEntry} width={"30px"}>Add</Button>
+                    </Card>
+                </Row>}
             </Container>
         </>
     );
-
 }
 
 export default Drivers;
