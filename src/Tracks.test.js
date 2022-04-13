15d7150f7218345620b76fd3e1f240afcac45b25
Noel <noel.gebhardt@gmail.com>
2022-04-13 17:28:16 +0200

    added tests & wireframes to documentation

--- src/Tracks.test.js
+++ src/Tracks.test.js
@@ -1,37 +1,39 @@
 import {cleanup, render, screen} from "@testing-library/react";
-import Teams from "./Teams";
 import {BrowserRouter} from "react-router-dom";
+import Tracks from "./Tracks";
 
-describe("<Teams />", () => {
+describe("<Tracks />", () => {
 
     afterEach(cleanup);
 
     it('should render without crashing', function () {
-        render(<BrowserRouter><Teams/></BrowserRouter>);
+        render(<BrowserRouter><Tracks/></BrowserRouter>);
     });
 
     it('should have insert button enabled', function () {
-        render(<BrowserRouter><Teams/></BrowserRouter>);
+        render(<BrowserRouter><Tracks/></BrowserRouter>);
 
         expect(screen.getByRole('button', {name: /insert/i})).toBeEnabled();
     });
 
     it('should have delete button disabled', function () {
-        render(<BrowserRouter><Teams/></BrowserRouter>);
+        render(<BrowserRouter><Tracks/></BrowserRouter>);
 
         expect(screen.getByRole('button', {name: /delete/i})).toBeDisabled();
     });
 
     it('should check if Links are visible', function () {
-        render(<BrowserRouter><Teams/></BrowserRouter>);
+        render(<BrowserRouter><Tracks/></BrowserRouter>);
 
         const linkElement = screen.getByText(/Drivers/i);
         expect(linkElement).toBeInTheDocument();
 
-        const linkElement2 = screen.getByRole('link', {name: /teams •/i});
+        const linkElement2 = screen.getByText(/Teams/i);
         expect(linkElement2).toBeInTheDocument();
 
-        const linkElement3 = screen.getByText(/Tracks/i);
+        const linkElement3 = screen.getByRole('link', {
+            name: /tracks •/i
+        })
         expect(linkElement3).toBeInTheDocument();
     });
 });
